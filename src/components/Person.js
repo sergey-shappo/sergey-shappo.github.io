d89ee1b5aac6a190b0e62b8905a53623f1e45974
Sergey Shappo <SergeyS@gocurb.com>
2020-05-15 20:56:34 +0200

    fix style bug

--- src/components/Person.js
+++ src/components/Person.js
@@ -15,13 +15,17 @@ const Person = ({id, top, left, hideIndex}) => {
     } = new PersonConfig(gedcomService.results[id]);
 
     return (
-        <div id={id} className={`${styles.personBox} ${styles.disableSelection} ${'hide' + hideIndex} ${position['left' + left]} ${position['top' + top]}`}>
-            <div className={styles.personLine}>{firstName}</div>
-            <div className={`${styles.personLine} ${styles.hidded}`}>{middleName}</div>
-            <div className={styles.personLine}>{lastName}</div>
-            {maidenName && maidenName !== lastName && <div className={`${styles.personLine} ${styles.hidded}`}>({maidenName})</div>}
-            <div className={`${styles.personLine} ${styles.hidded}`}>{dateOfBirth}</div>
-            <div className={`${styles.personLine} ${styles.hidded}`}>{dateOfDeath}</div>
+        <div className={styles.personBoxWrapper}>
+            <div id={id}
+                 className={`${styles.personBox} ${styles.disableSelection} ${'hide' + hideIndex} ${position['left' + left]} ${position['top' + top]}`}>
+                <div className={styles.personLine}>{firstName}</div>
+                <div className={`${styles.personLine} ${styles.hidded}`}>{middleName}</div>
+                <div className={styles.personLine}>{lastName}</div>
+                {maidenName && maidenName !== lastName &&
+                <div className={`${styles.personLine} ${styles.hidded}`}>({maidenName})</div>}
+                <div className={`${styles.personLine} ${styles.hidded}`}>{dateOfBirth}</div>
+                <div className={`${styles.personLine} ${styles.hidded}`}>{dateOfDeath}</div>
+            </div>
         </div>
     );
 };
